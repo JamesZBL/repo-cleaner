ac137d0ad0ec632911f546a7c365d54102a90b20
JamesZBL <1146556298@qq.com>
2019-09-01 09:53:50 +0800

    find dependencies from projects' base dir

--- clean.py
+++ clean.py
@@ -1,61 +1,89 @@
 from pathlib import Path
 from functools import reduce
-import os
+import os, subprocess, re
 
-PROJECTS_HOME = '~/repo'
+PROJECTS_HOME = '/Users/james/r'
 MAVEN_HOME = Path.home().joinpath('.m2')
 REPOSITORY_HOME = '{}/repository'.format(MAVEN_HOME)
 # REPOSITORY_HOME = '.tmp'
 
-repo_home = Path(REPOSITORY_HOME)
-artifacts = ['org.spring.boot:9.9.9.RELEASE:compile', 'org.spring.boot:9.9.10.RELEASE:compile']
-in_use_artifact_dirs = []
-for artifact in artifacts:
-  elements = artifact.split(':')
-  artifact_id = elements[0]
-  version = elements[1]
-  scope = artifact[2]
-  artifact_id_dir = artifact_id.replace('.', '/')
-  artifact_dir = '{}/{}'.format(artifact_id_dir, version)
-  in_use_artifact_dirs.append(artifact_dir)
-
-existing_artifact_dirs = []
-for root, folders, files in os.walk(REPOSITORY_HOME):
-  for file in files:
-    if bool(file.find('.pom')):
-      relative_path = str(Path(root).relative_to(REPOSITORY_HOME))
-      existing_artifact_dirs.append(relative_path)
-
-existing_artifact_dirs = list(set(existing_artifact_dirs))
-existing_artifact_dirs.sort()
-
-for d in existing_artifact_dirs:
-  for dd in existing_artifact_dirs:
-    if d.startswith(dd):
-      existing_artifact_dirs.remove(dd)
+def artifact_relative_dirs(artifact_coordinates):
+  result = []
+  for artifact in artifacts:
+    elements = artifact.split(':')
+    artifact_id = elements[0]
+    version = elements[1]
+    artifact_id_dir = artifact_id.replace('.', '/')
+    artifact_dir = '{}/{}'.format(artifact_id_dir, version)
+    result.append(artifact_dir)
+  return result
 
 def find_root_by_file_name(base_dir, file_name_section, reserve_mode='child'):
   result = []
-  for root, folders, files in os.walk(REPOSITORY_HOME):
+  for root, folders, files in os.walk(base_dir):
     for file in files:
-      if(bool(file.find('.pom'))):
-        relative_path = str(Path(root).relative_to(REPOSITORY_HOME))
+      if -1 < file.find(file_name_section):
+        relative_path = str(Path(root).relative_to(base_dir))
         result.append(relative_path)
   result = list(set(result))
   result.sort()
   if 'child' == reserve_mode:
     for d in result:
       for dd in result:
-        if d.startswith(dd):
+        if d != dd and d.startswith(dd):
           result.remove(dd)
   elif 'parent' == reserve_mode:
-    for dd in result:
-      for d in result:
-        if d.startswith(dd):
-          result.remove(d)
+    trash = []
+    for d in result:
+      for dd in result:
+        if d != dd and dd.startswith('{}/'.format(d)):
+          trash.append(dd)
+    filterd = []
+    for d in result:
+      if d not in trash:
+        filterd.append(d)
+    result = filterd
   return result
+
+def find_in_use_artifacts():
+  project_dirs = find_root_by_file_name(PROJECTS_HOME, 'pom.xml', 'parent')
+  result = []
+  for project_home in project_dirs:
+    print(project_home)
+    absolute_path = str(Path(PROJECTS_HOME).joinpath(project_home))
+    os.chdir(absolute_path)
+    process = subprocess.Popen('mvn dependency:tree', stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    out = str(out)
+    code = process.wait()
+    lines = out.split('\\n')
+    artifacts = find_dependencies_from_lines(lines)
+    result.extend(artifacts)
+  return result
+
+def find_dependencies_from_lines(lines):
+  tree_title_patter = re.compile(r'maven-dependency-plugin\:.*\:tree.* @\sgen')
+  dependency_patter = re.compile(r'(\|\s)?(\+|\\)-\s(.*\..*\:.*\:(compile|provided|test|runtime|system|import))')
+  title_found = False
+  dependency_found = False
+  artifacs = []
+  for line in lines:
+    title_match = re.search(tree_title_patter, line)
+    if title_match is not None:
+      title_found = True
+    dependency_match = re.search(dependency_patter, line)
+    if dependency_match is not None:
+      dependency_found = True
+      regs = dependency_match.regs
+      start, end = regs[len(regs)-2]
+      artifacs.append(line[start:end])
+  return artifacs
+
+existing_artifact_dirs = find_root_by_file_name(REPOSITORY_HOME, '.pom', 'child')
+artifacts = find_in_use_artifacts()
+in_use_artifact_dirs = artifact_relative_dirs(artifacts)
 
 for i in existing_artifact_dirs:
   print(i)
 
-print(in_use_artifact_dirs)
+# print(in_use_artifact_dirs)
